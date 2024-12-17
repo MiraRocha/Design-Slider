5ab26139e3888ced58418bdea51f11fc269526ef
MiraRocha <MiraRocha@users.noreply.github.com>
2024-12-17 17:57:04 +0000

    +

---
+++ app.js
@@ -0,0 +1,5 @@
+nextDom = document.getElementById('next');
+prevDom = document.getElementById('prev');
+carouselDom = document.querySelector('.carousel');
+listItemDom = document.querySelector('.carousel .list');
+thumbnailDom = document.querySelector('.carousel .thumbnail')
